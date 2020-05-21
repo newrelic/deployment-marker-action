05d7bc367fd53bc9cabddd98a91489a2e499c5a5
Jonathan Thurman <jthurman@newrelic.com>
2020-05-21 08:00:42 -0700

    fix(errors): Echo using GH Error interface (resolves #3)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-RED='\033[0;31m'
-NO_COLOR='\033[0m'
-
 result=$(newrelic apm deployment create --applicationId "${APPLICATION_ID}" --revision "${REVISION}" --accountId "${NEW_RELIC_ACCOUNT_ID}" --user "${NEW_RELIC_DEPLOYMENT_USER}" 2>&1)
 
 exitStatus=$?
 
 if [ $exitStatus -ne 0 ]; then
-  printf "${RED}Error:${NO_COLOR} $result"
+  echo "::error:: $result"
 fi
 
 exit $exitStatus
