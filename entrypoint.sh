83dc82d38d24e67741c809eaae84fba4e7a107dc
Stephen Goodall <stephen.goodall88@googlemail.com>
2020-05-27 14:28:53 +1000

    Revert previous changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-result=$(newrelic apm deployment create \\
-  --accountId "${NEW_RELIC_ACCOUNT_ID}" \\
-  --applicationId "${NEW_RELIC_APPLICATION_ID}" \\
-  --user "${NEW_RELIC_DEPLOYMENT_USER}" \\
-  --revision "${NEW_RELIC_DEPLOYMENT_REVISION}" \\
-  --change-log "${NEW_RELIC_DEPLOYMENT_CHANGE_LOG}" \\
-  --description "${NEW_RELIC_DEPLOYMENT_DESCRIPTION}" \\
+result=$(newrelic apm deployment create \
+  --accountId "${NEW_RELIC_ACCOUNT_ID}" \
+  --applicationId "${NEW_RELIC_APPLICATION_ID}" \
+  --user "${NEW_RELIC_DEPLOYMENT_USER}" \
+  --revision "${NEW_RELIC_DEPLOYMENT_REVISION}" \
+  --change-log "${NEW_RELIC_DEPLOYMENT_CHANGE_LOG}" \
+  --description "${NEW_RELIC_DEPLOYMENT_DESCRIPTION}" \
   2>&1)
 
 exitStatus=$?
