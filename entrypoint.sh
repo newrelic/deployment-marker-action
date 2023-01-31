f378b12dbd50b12f00653c78e4a8281db9617001
Sander Blue <sblue@sanderblue.com>
2023-01-31 10:43:58 -0600

    feat!: v2.0.0 (#23)
    Co-authored-by: Justin Lewis <Justinlewis@newrelic.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-result=$(newrelic apm deployment create \
-  --accountId "${NEW_RELIC_ACCOUNT_ID}" \
-  --applicationId "${NEW_RELIC_APPLICATION_ID}" \
+result=$(newrelic entity deployment create \
+  --guid "${NEW_RELIC_DEPLOYMENT_ENTITY_GUID}" \
   --user "${NEW_RELIC_DEPLOYMENT_USER}" \
-  --revision "${NEW_RELIC_DEPLOYMENT_REVISION}" \
-  --change-log "${NEW_RELIC_DEPLOYMENT_CHANGE_LOG}" \
+  --version "${NEW_RELIC_DEPLOYMENT_VERSION}" \
+  --changelog "${NEW_RELIC_DEPLOYMENT_CHANGE_LOG}" \
   --description "${NEW_RELIC_DEPLOYMENT_DESCRIPTION}" \
+  --deepLink "${NEW_RELIC_DEPLOYMENT_DEEPLINK}" \
+  --deploymentType "${NEW_RELIC_DEPLOYMENT_TYPE}" \
+  --groupId "${NEW_RELIC_DEPLOYMENT_GROUP_ID}" \
   2>&1)
 
 exitStatus=$?
